84d66b932c396659716c8c6434423f5ef3c0f757
YaelMartin821 <ymartinxool@gmail.com>
2025-03-07 20:06:51 -0600

    Agregado archivo web.php

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,6 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Livewire\Boletos;
+use App\Livewire\Usuarios;
+
+Route::view('/usuarios', 'Usuarios.usuarios')->name('usuarios');
+Route::view('/boletos', 'Boletos.boletos')->name('boletos');
+
+
+
+
+
 
 Route::get('/', function () {
     return view('welcome');
