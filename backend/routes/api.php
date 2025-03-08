cea38c87f19b2b6c8d3b3aa9aefcb6f32f028255
YaelMartin821 <ymartinxool@gmail.com>
2025-03-07 20:07:45 -0600

    Agregado archivo api.php

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -6,3 +6,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+Route::get('/mostrar-boletos', 
+    [\App\Livewire\Boletos::class, 'index']
+)->name('mostrar-boleto');
