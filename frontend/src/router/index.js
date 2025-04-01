0ed17800f9ecb2959e8a7b61f676040a9fd5c826
RobertCCE <canulroberto36@gmail.com>
2025-04-01 10:58:21 -0600

    correcciones

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,32 +1,3 @@
-<<<<<<< HEAD
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import TipoTarifa from "@/views/TipoTarifa.vue";
-
-const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: "/Tipotarifa",
-      name: "tipotarifa",
-      component: TipoTarifa,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
-  ],
-})
-=======
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import BoletosView from "../views/BoletosView.vue";
@@ -57,6 +28,5 @@ const router = createRouter({
   history: createWebHistory(),
   routes,
 });
->>>>>>> 3e588dd204e12255f3b31d5a1b1b6ec3434c42ea
 
 export default router;
